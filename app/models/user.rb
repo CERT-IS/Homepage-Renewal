197f19e099f52675b44e4f6011bb9edb11035d73
kkd927 <kkd927@gmail.com>
2015-07-31 16:17:38 +0900

    Adjust new password template

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :uid, presence: true, uniqueness: { case_sensitive: false }
+  validates :realname ,presence: true
+  validates :phone ,presence: true, length: { is: 13 }
 end
