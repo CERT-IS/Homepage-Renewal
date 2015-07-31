613dc1aecf61ffe565d375de4e97170461ebb536
kkd927 <kkd927@gmail.com>
2015-07-31 22:43:07 +0900

    Fix New Password defualt url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Set default URL options for the Devise mailer
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'www.cert-is.ga', port: 4389 }
 end
