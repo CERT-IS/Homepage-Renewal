b5630537a1ebe932832551a959a898da06337eec
kkd927 <kkd927@gmail.com>
2015-11-03 19:27:07 +0900

    Change logo

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -4,11 +4,11 @@ module API
 			authenticate_or_request_with_http_basic do |uid, password|
 				user = User.find_by_uid(uid)
 				head 401 unless user.present?
-				
+
 				if user.valid_password?(password)
 					render json: user, status: 200
 				else
-					head :401
+					head 401
 				end
 			end
 		end
