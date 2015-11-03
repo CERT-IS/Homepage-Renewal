d15ed8f9e370eb92f2d78e158d75fbb603ad3c82
kkd927 <kkd927@gmail.com>
2015-11-03 19:36:44 +0900

    Bugfix basic auth uid not found

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -3,10 +3,13 @@ module API
 		def get_token
 			authenticate_or_request_with_http_basic do |uid, password|
 				user = User.find_by_uid(uid)
-				head 401 unless user.present?
-
-				if user.valid_password?(password)
-					render json: user, status: 200
+				
+				if user.present?
+					if user.valid_password?(password)
+						render json: user, status: 200
+					else
+						head 401
+					end
 				else
 					head 401
 				end
