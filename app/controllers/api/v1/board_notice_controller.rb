a6bde5fb7104ffa601657f89bcce0f8c4f589cde
GyeongDeok Kwon <kkd927@gmail.com>
2015-11-27 05:00:34 +0900

    Fix notice index only allow json request

--- app/controllers/api/v1/board_notice_controller.rb
+++ app/controllers/api/v1/board_notice_controller.rb
@@ -9,9 +9,8 @@ module API
 				@boards  	= @type.boards
 				@page_max	= (@boards.count-1) / 4 + 1 if @boards.present?
 				@boards 	= @boards.paginate(:page => params[:page], :per_page => 4)
-				
+
 				respond_to do |format|
-					format.html
 					format.json { render json: @boards, except: [:contents, :updated_at, :user_id], include: {user:{only: :uid}}, status: 200 } 
 				end
 			end
