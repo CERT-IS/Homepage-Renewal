84ee2b807f9b46487d2768f6b5bd7438b39e1744
GyeongDeok Kwon <kkd927@gmail.com>
2015-11-27 13:54:35 +0900

    Fix authenticat or request with http token

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -27,7 +27,7 @@ module API
 			end
 
 			def authenticate_token!
-				authenticate_with_http_token do |token, options|
+				authenticate_or_request_with_http_token do |token, options|
 					user = User.find_by(authentication_token: token)
 
 					unless user.present? and user.member?
