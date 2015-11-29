5b1fff671eca971b47ab571b5b5755811dc4a038
D20K <kkd927@gmail.com>
2015-11-29 15:20:21 +0900

    Bugfix reutrn explicit in authenticate token

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -24,14 +24,14 @@ module API
 			def authenticate_token
 				authenticate_with_http_token do |token, options|
 					user = User.find_by(authentication_token: token)
-					user.present? and user.member?
+					return (user.present? and user.member?)
 				end
 			end
 
 			def render_unauthorized
-		      	self.headers['WWW-Authenticate'] = 'Token realm="Application"'
-		      	render json: 'Bad credentials', status: 401
-		    end
+		      		self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+		      		render json: 'Bad credentials', status: 401
+		    	end
 
 			def check_format
 				render :nothing => true, :status => 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
