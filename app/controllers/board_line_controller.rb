63a4f06975d1a8715e611a2aaa710e1518b86735
GyeongDeok Kwon <kkd927@gmail.com>
2015-12-04 16:46:13 +0900

    Add board_line loadmore

--- app/controllers/board_line_controller.rb
+++ app/controllers/board_line_controller.rb
@@ -1,13 +1,18 @@
 class BoardLineController < BoardsController
 	def index
 		super
-		@page   	= params[:page].to_i == 0 ? 1 : params[:page].to_i
+		@page   	= params[:page].present? ? params[:page].to_i : 1
 		@type   	= BoardType.where(name: "lines").first
 		@boards  	= @type.boards
 		@page_max	= (@boards.count-1) / 4 + 1 if @boards.present?
 		@boards 	= @boards.paginate(:page => params[:page], :per_page => 4)
 
 		@board 		= @type.boards.new
+
+		respond_to do |format|
+			format.html
+			format.js
+		end
 	end
 
 	def create
