04d228fa5ad717ecfe8457b6a38f5be03722d4a9
GyeongDeok Kwon <kkd927@gmail.com>
2016-01-04 22:01:47 +0900

    Fix board api page per 6

--- app/controllers/api/v1/board_notice_controller.rb
+++ app/controllers/api/v1/board_notice_controller.rb
@@ -7,8 +7,8 @@ module API
 				@page   	= params[:page].to_i == 0 ? 1 : params[:page].to_i
 				@type   	= BoardType.where(name: "notices").first
 				@boards  	= @type.boards
-				@page_max	= (@boards.count-1) / 4 + 1 if @boards.present?
-				@boards 	= @boards.paginate(:page => params[:page], :per_page => 4)
+				@page_max	= (@boards.count-1) / 6 + 1 if @boards.present?
+				@boards 	= @boards.paginate(:page => params[:page], :per_page => 6)
 			end
 		end
 	end
