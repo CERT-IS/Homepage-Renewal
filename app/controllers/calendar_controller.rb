8f74688922f8b2aabb43d3b6a8303cc11df10212
kkd927 <kkd927@gmail.com>
2016-01-19 15:09:38 +0900

    Add calendar index events show

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,6 +2,7 @@ class CalendarController < ApplicationController
 	before_action :authenticate_user!
 	
 	def index
+		@events = Event.get_events
 	end
 
 	def create
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ActiveRecord::Base
 	validates :title, presence: true
 	validates :start_day, presence: true
 	validates :end_day, presence: true
+
+	def self.get_events
+		where.not(
+			'(end_day <= ? AND start_day >= ?)',
+			Date.today.prev_month,
+			Date.today.next_month
+		)
+	end
 end
