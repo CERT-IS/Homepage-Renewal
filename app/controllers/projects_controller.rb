8788cf8d8ddd700d2c3eb293c0d030d15e258ff7
kkd927 <kkd927@gmail.com>
2016-01-21 13:59:00 +0900

    Add Project#Create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :get_project, only: [:show]
 
 	def index
 	end
@@ -9,9 +10,24 @@ class ProjectsController < ApplicationController
 	end
 
 	def create
+		@project = current_user.projects.new(project_params)
+		if @project.save
+			UserProjectMapped.create(
+				user: current_user,
+				project: @project,
+				master: true
+			)
+			redirect_to @project
+		else
+			render 'new'
+		end
 	end
 
 	def show
+		@project_master = 	@project.users.where(
+								"user_project_mappeds.master = ?",
+								true
+							).first
 	end
 
 	def edit
@@ -21,5 +37,20 @@ class ProjectsController < ApplicationController
 	end
 
 	def destory
+	end
+
+	private
+
+	def get_project
+		@project = Project.where(id: params[:id]).first
+	end
+
+	def project_params
+		params.require(:project).permit(
+			:title,
+			:description,
+			:start_date,
+			:end_date
+		)
 	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
 	has_many :user_project_mappeds
 	has_many :users, through: :user_project_mappeds
+
+	validates :title, presence: true
+    validates :description, presence: true
+    validates :start_date, presence: true
+    validates :end_date, presence: true
 end
