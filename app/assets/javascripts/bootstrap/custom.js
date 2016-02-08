9914327b3e1d49deaf6fc127c462c05d27d37e96
GyeongDeok Kwon <kkd927@gmail.com>
2016-02-08 17:28:27 +0900

    Fix Board validations

--- app/assets/javascripts/bootstrap/custom.js
+++ app/assets/javascripts/bootstrap/custom.js
@@ -13,13 +13,40 @@ function registerAllow() {
 }
 
 function summernote_submit() {
-	var title = $('#board_title').val();
-	if (!title) {
-		$('#board-title').addClass("has-error");
-		$('#board-title').append('<p style="color:red">제목을 입력하세요.</p>')
+	var title      = $('#board_title').val();
+	var body_empty = $('#summernote').code().length == 0
+
+	if ( !title || body_empty )
+	{
+		if ( !title ) 
+		{
+			if ( !$('#title-error').length )
+			{
+				$('#board-title').addClass("has-error");
+				$('#board-title').append('<p id="title-error" style="color:red">제목을 입력하세요.</p>');
+			}
+		}
+		else
+		{
+			$('#board-title').removeClass("has-error");
+			$('#title-error').remove();
+		}
+
+		if ( body_empty ) 
+		{
+			if ( !$('#body-error').length )
+			{
+				$('#content_head').addClass("has-error");
+				$('#content_head').after('<p id="body-error" style="color:red">본문을 입력하세요.</p>');
+			}
+		}
+		else
+		{
+			$('#content_head').removeClass("has-error");
+			$('#body-error').remove();
+		}
+
 		$("html, body").animate({ scrollTop: 0 }, "slow");
 		event.preventDefault();
 	}
-	var t =  $('#summernote').code();
-    $('#board_contents').val(t);
 }
